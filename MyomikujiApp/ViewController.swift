0bfb4a3c22daa671ec96b829b98419961b6ed263
管原秀太 <sugawarasyuta@sugawarashuuta-no-MacBook-Air.local>
2017-08-31 15:46:41 +0900

    Initial Commit

---
+++ MyomikujiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyomikujiApp
+//
+//  Created by 管原秀太 on 2017/08/31.
+//  Copyright © 2017 管原秀太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
