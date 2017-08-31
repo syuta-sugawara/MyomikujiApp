a81b433e2b44b2c40cf5b81fffab68770b52b595
管原秀太 <sugawarasyuta@sugawarashuuta-no-MacBook-Air.local>
2017-08-31 18:10:45 +0900

    first commit

--- MyomikujiApp/ViewController.swift
+++ MyomikujiApp/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func getOmikuji(_ sender: Any) {
+        let results = ["大吉","吉","中吉","凶","大凶"]
+        let random = arc4random_uniform(UInt32(results.count))
+         self.myLabel.text = results[Int(random)]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
